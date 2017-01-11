908c5c55590477a9bd77ecfc79a4bdd1e0de4e7b
axel <axel.bouillart@gmail.com>
2017-01-11 10:12:33 +0100

    exo 4

---
+++ array/array-4.php
@@ -0,0 +1,21 @@
+<?php
+
+$mois =[
+
+"janvier",
+"février",
+"mars",
+"avril",
+"mai",
+"juin",
+"juillet",
+"aôut",
+"septembre",
+"octobre",
+"novembre",
+"décembre",
+ 
+];
+
+echo $mois[7];
+?>
