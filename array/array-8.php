23f239d494e3bb206f27922cbdab7fce1332a0d9
axel <axel.bouillart@gmail.com>
2017-01-11 11:08:43 +0100

    exo 8

---
+++ array/array-8.php
@@ -0,0 +1,26 @@
+<?php
+
+
+$mois =[
+
+"janvier",
+"février",
+"mars",
+"avril",
+"mai",
+"juin",
+"juillet",
+"aout",
+"septembre",
+"octobre",
+"novembre",
+"décembre",
+ 
+];
+
+foreach ($mois as $value) {
+	echo $value ." ";
+}
+
+echo $mois;
+?>
