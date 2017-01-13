697817c6e783900a15f988c88dee0d4099159ea5
axel <axel.bouillart@gmail.com>
2017-01-13 09:33:17 +0100

    exo 3

---
+++ varSuperGlobale/varSuper-2/index.php
@@ -0,0 +1,19 @@
+
+<?php  session_start();
+
+$_SESSION['nom']= 'Axel';
+$_SESSION['prenom']= 'Bouillart';
+$_SESSION['age']= '19';
+
+?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="UTF-8">
+	<title>Document</title>
+</head>
+<body>
+<a href="user.php">Name</a>
+	
+</body>
+</html>
---
+++ varSuperGlobale/varSuper-2/user.php
@@ -0,0 +1,4 @@
+<?php session_start();?>
+
+
+<h1><?php echo $_SESSION['nom'] .' '. $_SESSION['prenom'] .' '. $_SESSION['age']?></h1>
