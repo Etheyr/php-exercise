e40c1769f4ce2037c7b1f0aa550a604eff3294db
axel <axel.bouillart@gmail.com>
2017-01-16 15:23:57 +0100

    exo 2 form en POO

---
+++ formPOO/formPOO-2/index.php
@@ -0,0 +1,33 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="UTF-8">
+	<title>form avec POO</title>
+	<link rel="stylesheet" href="https://cdn.jsdelivr.net/semantic-ui/2.2.6/semantic.min.css">
+	<link rel="stylesheet" type="text/css" href="style.css">
+</head>
+<body>
+
+	
+	<form action="user.php" method="post">
+		<div id="sizeForm">
+			<div class="ui  segment">
+				<div class="ui  form">
+					<div class="field">
+						<label id="labelI">Fist name</label>
+						<input id="inputC" name="firstName" type="text">
+					</div>
+					<div class="field">
+						<label id="labelI">Last name</label>
+						<input id="inputC" name="lastName" type="text">
+					</div>
+				</div>
+				<br>
+				<button class="ui blue button" name="submit" type="submit">Submit</button>
+			</div>
+		</div>
+	</form>
+
+
+</body>
+</html>
